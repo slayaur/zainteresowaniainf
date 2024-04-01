483d4d67bb2cc1bc3a9216dc0a80a97a10d7d24d
Gabriela Skiba <gabrielaa.skiba@gmail.com>
2024-04-02 00:54:28 +0200

    async

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,42 +1,36 @@
 const apiKey = '333707842e574ab3911155106240104';
-const city = 'Warsaw'; 
+const city = 'Warsaw';
 
 const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
-function updateWeater(){
-    fetch(apiUrl, {  method: "GET",  mode: "no-cors"})
-    .then(response => {
-        // if (!response.ok) {
-        //     console.error('Serwis zwrócił błąd', response);
-        //     throw new Error('Serwis zwrócił błąd');
-        // }
-        return response.json();
-    })
-    .then(jason => {
+async function updateWeater() {
+    try {
+        let response = await fetch(apiUrl, { method: "GET", mode: "no-cors" });
+        let jason = await response.json();
+
         const weatherDiv = document.getElementById('weather');
-        
+
         const temperature = jason.current.temp_c + "°C";
         const conditionText = jason.current.condition.text;
         const icon = jason.current.condition.icon;
 
         // Ustawienie tekstu z temperaturą
         const conditionTextElements = weatherDiv.getElementsByClassName("condition-text");
-        conditionTextElements[0].innerHTML = temperature;       
-
-        
-
+        conditionTextElements[0].innerHTML = temperature;
 
         // Ustawienie ikonki z temperaturą
         const iconElement = document.getElementById("weather-icon");
-        iconElement.src = icon;        
-        
-    })
-    .catch(error => {
+        iconElement.src = icon;
+
+    } catch (error) {
+
         console.error('Wystąpił problem podczas pobierania pogody', error);
-    })
+
+    }
+
 };
 
-function updateTime(){
+function updateTime() {
     const dateElement = document.getElementById("current-time");
     dateElement.innerHTML = (new Date().toLocaleTimeString());
 }
